23f6fa0b583f96e2d5b05017f83f8bfee832a383
Tomeksam <tomeksamule@gmail.com>
2025-05-15 00:52:23 +0200

    advanced.point.py

--- advanced.point.py
+++ advanced.point.py
@@ -3,52 +3,71 @@ from colour_point2 import ColourPoint, PointException
 
 class AdvancedPoint(ColourPoint): # lets define a lsit of acceptable colours
     COLOURS = ["red", "blue", "green", "yellow", "black", "white", "periwinkle"] # not a attribute per instance, its the same for each class
+
     def __init__(self, x, y, colour):
+        """
+        Constructor: Validates input colour and sets x, y, and colour as private attributes.
+        """
         if colour not in self.COLOURS:
             raise PointException(f"Invalid colour bro must be one of the ones i{self.COLOURS}")
         self._x = x
         self._y = y
         self._colour = colour
 
-    @property # allows for function without the brackts? with a hidden value of x, method that doesnt take any other parameters
+    @property 
     def x(self):
+        """
+        Getter: Returns x-coordinate.
+        """
         return self._x # getter method
 
-    @property  # allows for function without the brackts? with a hidden value of x, method that doesnt take any other parameters
+    @property  
     def x(self, value):
+        """
+        Setter: Updates x-coordinate to new value.
+        """
         self._x = value  # setter method
 
     @property
     def y(self):
+        """
+        Getter: Returns y-coordinate.
+        """
         return self._y
 
     @property
     def colour(self):
+        """
+        Getter: Returns colour attribute.
+        """
         return self._colour
 
-    @classmethod #a method that applies to the class as a whole, matter where it will apply as a whole
+    @classmethod 
     def add_colour(cls, colour):
         """
-        adds a new valid colour for our class
-        :param colour:
-        :return:
+        Class method: Adds a new allowed colour to COLOURS list.
         """
         cls.COLOURS.append(colour)
-    @staticmethod # functions that have nothing to do with the parameters of class, affects the class output not inputs,
+
+    @staticmethod 
     def from_tuple(coordinate, colour="red"):
         """
-        creates a new point from a tuple rather than 2 individual values
-        :param coordinate:
-        :param colour:
-        :return:
+        Static method (factory): Creates instance from coordinate tuple and optional colour.
         """
         x, y = coordinate
         return AdvancedPoint(x, y, colour)
+
     @staticmethod
     def distance_2_points(p1, p2):
-        return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) **2)**0.5
+        """
+        Static method: Returns Euclidean distance between two point instances.
+        """
+        return ((p1.x - p2.x) ** 2 + (p1.y - p2.y)**2)**0.5
 
     def distance_to_other(self, p):
+        """
+        Instance method: Returns distance from current object to another point.
+        """
         return ((self.x - p.x) ** 2 + (self.y - p.y)** 2) ** 0.5
 
 
@@ -61,4 +80,3 @@ p2 = AdvancedPoint.from_tuple((3,2))
 print(p2)
 print(AdvancedPoint.distance_2_points(p,p2))
 print(p.distance_to_other(p2))
-
