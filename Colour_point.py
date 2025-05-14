fcf40796965eb934dc08ed93ab0cb38be05ae09a
Tomeksam <tomeksamule@gmail.com>
2025-05-15 00:54:04 +0200

    Colour_point.py

--- Colour_point.py
+++ Colour_point.py
@@ -1,27 +1,32 @@
+from point import Point
 import random
 
-from point import Point
-
-class ColourPoint(Point): #inheritence
+class ColourPoint(Point):
     def __init__(self, x, y, colour):
-        self.x = x
-        self.y = y
-        self.colour = colour
+        """
+        Init method:
+        - Creates a ColourPoint object from x, y coords and a colour string.
+        - Inherits x and y via super() from the Point class.
+        - Raises TypeError if x or y are not numeric types.
+        """
+        if not isinstance(x, (int, float)): # x, tuple; isinstance asks what is the type of x
+            raise TypeError("x must be a number")
+        if not isinstance(y, (int, float)):
+            raise TypeError("y must be a number")
+
+        super().__init__(x, y) # access parent class through super() (do not have to write self.x & self.y again); replaces self.x & self.y
+        self.colour = colour # edit __init__ & everything else carries over from Point
 
     def __str__(self):
+        """
+        Dunder method (__str__):
+        - Defines how object prints when passed to print().
+        - Returns colour + coords in string format.
+        """
         return f"<{self.colour}: {self.x}, {self.y}>"
 
 if __name__ == "__main__":
-    p = ColourPoint(1,2,"red")
-    print(p)
-    colours = ["red", "green", "blue", "yellow", "black", "magenta", "cyan", "white", "burgundy", "periwinkle", "marsala"]
-    colour_points = []
-    for i in range(10):
-        colour_points.append(
-            ColourPoint(random.randint(-10,10),
-                        random.randint(-10,10), #third parameter, colour
-                        random.choice(colours)))
+    p = ColourPoint(1, 2, "red")
 
-    print(colour_points)
-    colour_points.sort()
-    print(colour_points)
+    print(p.distance_to_orig()) # inherited method
+    print(p)
